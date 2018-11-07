6d2f85d0455171e844563cd5c0e8922be604e9b8
Scott Huffman <scott@shuffman.com>
2018-11-07 16:34:52 -0600

    All 3 algorithms implemented

--- WGraph.java
+++ WGraph.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.PriorityQueue;
@@ -19,7 +18,6 @@ public class WGraph {
     	private int x;
     	private int y;
     	private ArrayList<Edge> edges;
-    	private boolean dst = false;
     	
         Vertex(int x, int y){
         	this.x = x;
@@ -35,15 +33,6 @@ public class WGraph {
         	this.edges.add(e);
         }
         
-        public boolean isDst(){
-        	return dst;
-        }
-        public void setDst() {
-        	this.dst = true;
-        }
-        public void resetDst() {
-        	this.dst = false;
-        }
     }
     private class Edge{
     	Vertex dst;
@@ -80,7 +69,6 @@ public class WGraph {
     		int dy = scanner.nextInt();
     		int weight = scanner.nextInt();
     		
-    		//System.out.println(sx + " " + sy + " " + dx + " " + dy + " " + weight);
     		
     		String srcKey = "" + sx + sy;
     		Vertex src = lookup.get(srcKey);
@@ -100,10 +88,7 @@ public class WGraph {
     		src.addEdge(new Edge(dst, weight));
     		
     	}
-//    	for (String key : lookup.keySet()) {
-//    	    System.out.println(""+key + " - " + lookup.get(key).x + " " + lookup.get(key).y);
-//    	}
-    	
+
     	scanner.close();
     }
 
@@ -120,46 +105,13 @@ public class WGraph {
     ArrayList<Integer> V2V(int ux, int uy, int vx, int vy){
     	Vertex src = this.lookup.get(""+ux+uy);
     	Vertex dst = this.lookup.get(""+vx+vy);
-//    	dst[0].setDst();
-    	
-    	//do dijkstra's
+
     	dijkstra(src, dst);
-    	
-//    	dst.resetDst();
+
     	return new ArrayList<Integer>();
     }
     
-    private void dijkstra(Vertex src, Vertex dst) {
-//    	1) Initialize distances of all vertices as infinite.
-//
-//    2) Create an empty priority_queue pq.  Every item
-//       of pq is a pair (weight, vertex). Weight (or 
-//       distance) is used used as first item  of pair
-//       as first item is by default used to compare
-//       two pairs
-//
-//    3) Insert source vertex into pq and make its
-//       distance as 0.
-//
-//    4) While either pq doesn't become empty
-//        a) Extract minimum distance vertex from pq. 
-//           Let the extracted vertex be u.
-//        b) Loop through all adjacent of u and do 
-//           following for every vertex v.
-//
-//               // If there is a shorter path to v
-//               // through u. 
-//               If dist[v] > dist[u] + weight(u, v)
-//
-//                   (i) Update distance of v, i.e., do
-//                         dist[v] = dist[u] + weight(u, v)
-//                   (ii) Insert v into the pq (Even if v is
-//                        already there)
-//                   
-//    5) Print distance array dist[] to print all shortest
-//       paths. 
-    	
-    	
+    private ArrayList<Vertex> dijkstra(Vertex src, Vertex dst) {
     	HashMap<Vertex, Integer> dist = new HashMap<Vertex, Integer>(this.vertCount);
     	HashMap<Vertex, Vertex> parent = new HashMap<Vertex, Vertex>(this.vertCount);
     	
@@ -172,7 +124,7 @@ public class WGraph {
     	
     	//algo begin
     	dist.put(src, 0);
-    	parent.put(src, null);
+    	parent.put(null, src);
     	
     	q.add(new Tuple<Vertex, Integer>(src, 0));
     	
@@ -180,12 +132,9 @@ public class WGraph {
     	    if(value != src) {
     	    	dist.put(value, Integer.MAX_VALUE);
     	    }
-//    	    System.out.println("" + value.x + value.y);
     	}
     	
-//    	for (Vertex key : dist.keySet()) {
-//    	    System.out.println(""+key.x + key.y + " - " + dist.get(key));
-//    	}
+
     	
     	while(q.peek() != null) {
     		Tuple<Vertex, Integer> t = q.poll();
@@ -196,20 +145,22 @@ public class WGraph {
     			Vertex v = e.dst;
     			if(dist.get(v) > dist.get(u) + e.weight) {
     				dist.replace(v, dist.get(u) + e.weight);
-    				parent.put(v, u);
+    				parent.put(u, v);
     				q.add(new Tuple<Vertex, Integer>(v, dist.get(v)));
     			}
     		}
     	}
     	
-    	for (Vertex key : dist.keySet()) {
-    	    System.out.println(""+key.x + key.y + " - " + dist.get(key));
-    	}
-    	Vertex next = dst;
-    	while(next != null) {
-    		System.out.println("" + next.x + next.y);
+
+    	Vertex next = null;
+    	ArrayList<Vertex> path = new ArrayList<Vertex>();
+    	while(next != dst) {
     		next = parent.get(next);
+    		System.out.println("" + next.x + next.y);
+    		path.add(next);
     	}
+    	
+    	return path;
     	
     }
 
@@ -225,17 +176,15 @@ public class WGraph {
     ArrayList<Integer> V2S(int ux, int uy, ArrayList<Integer> S){
     	Vertex src = this.lookup.get(""+ux+uy);
     	
-    	for(int i = 0; i < S.size(); i++) {
-    		this.lookup.get(""+S.get(i)+S.get(i+1)).setDst();
+    	Vertex s = new Vertex(-1, -1);
+    	this.lookup.put("-1-1", s);
+    	for(int i = 0; i < S.size(); i+=2) {
+    		Edge e = new Edge(s, 0 );
+    		this.lookup.get(""+S.get(i)+S.get(i+1)).addEdge(e);
     	}
     	
-    	//do dijkstra's
+    	dijkstra(src, s);
     	
-    	
-    	
-    	for(int i = 0; i < S.size(); i++) {
-    		this.lookup.get(""+S.get(i)+S.get(i+1)).resetDst();
-    	}
     	return new ArrayList<Integer>();
     }
 
@@ -246,170 +195,26 @@ public class WGraph {
     //       the representation of a set of vertices as arrayList)
     // post: same structure as the last method’s post.
     ArrayList<Integer> S2S(ArrayList<Integer> S1, ArrayList<Integer> S2){
+    	//Vertex src = this.lookup.get(""+ux+uy);
+    	
+    	Vertex s1 = new Vertex(-1, -1);
+    	this.lookup.put("-1-1", s1);
+    	for(int i = 0; i < S1.size(); i+=2) {
+    		Edge e = new Edge(this.lookup.get(""+S1.get(i)+S1.get(i+1)), 0 );
+    		s1.addEdge(e);
+    	}
+    	
+    	Vertex s2 = new Vertex(-2, -2);
+    	this.lookup.put("-2-2", s2);
+    	for(int i = 0; i < S2.size(); i+=2) {
+    		Edge e = new Edge(s2, 0 );
+    		this.lookup.get(""+S2.get(i)+S2.get(i+1)).addEdge(e);
+    	}
+    	
+    	dijkstra(s1, s2);
+    	
     	return new ArrayList<Integer>();
     }
-    
-    
-//    
-//    
-//    
-//    /**
-//     * A priority queue.
-//     *
-//     * @author Scott Huffman
-//     * @author John Jago
-//     */
-//    public class PriorityQ<T> {
-//    	public class PriorityVal<T> {
-//
-//    	    private String str;
-//    	    private int priority;
-//
-//    	    public PriorityVal(String str, int priority) {
-//    	        this.str = str;
-//    	        this.priority = priority;
-//    	    }
-//
-//    	    @Override
-//    	    public boolean equals(Object o) {
-//    	        if (this == o)
-//    	            return true;
-//
-//    	        if (o == null)
-//    	            return false;
-//
-//    	        if (getClass() != o.getClass())
-//    	            return false;
-//
-//    	        PriorityVal<T> other = (PriorityVal<T>) o;
-//    	        return this.str.equals(other.str) && this.priority == other.priority;
-//    	    }
-//
-//    	    public String getStr() {
-//    	        return str;
-//    	    }
-//
-//    	    public void setStr(String str) {
-//    	        this.str = str;
-//    	    }
-//
-//    	    public int getPriority() {
-//    	        return priority;
-//    	    }
-//
-//    	    public void setPriority(int priority) {
-//    	        this.priority = priority;
-//    	    }
-//    	}
-//
-//        private PriorityVal[] data;
-//        private int size;
-//
-//        public PriorityQ() {
-//            data = new PriorityVal[100];
-//            size = 0;
-//        }
-//
-//        public void add(String s, int p) {
-//        	PriorityVal newString = new PriorityVal(s, p);
-//            data[size] = newString;
-//            size++;
-//
-//            if (size > data.length - 1) {
-//            	PriorityVal[] newData = new PriorityVal[data.length * 2];
-//                System.arraycopy(data, 0, newData, 0, data.length);
-//                data = newData;
-//            }
-//
-//            int i = size - 1;
-//            while (i != 0 && newString.getPriority() > data[(i - 1) / 2].getPriority()) {
-//                data[i] = data[(i - 1) / 2];
-//                i = (i - 1) / 2;
-//            }
-//
-//            data[i] = newString;
-//        }
-//
-//        public PriorityVal returnMax() {
-//            return data[0];
-//        }
-//
-//        public PriorityVal extractMax() {
-//        	PriorityVal max = returnMax();
-//            data[0] = data[size - 1];
-//            size--;
-//            percolateDown(data, size, 0);
-//
-//            return max;
-//        }
-//
-//        public void remove(int i) {
-//            swap(i, size - 1);
-//            size--;
-//            percolateDown(data, size, i);
-//        }
-//
-//        public void decrementPriority(int i, int k) {
-//            data[i].setPriority(data[i].getPriority() - k);
-//            percolateDown(data, size, i);
-//        }
-//
-//        public int[] priorityArray() {
-//            int[] temp = new int[size];
-//
-//            for (int i = 0; i < size; i++) {
-//                temp[i] = data[i].getPriority();
-//            }
-//
-//            return temp;
-//        }
-//
-//        public int getKey(int i) {
-//            return data[i].getPriority();
-//        }
-//
-//        public String getValue(int i) {
-//            return data[i].getStr();
-//        }
-//
-//        public boolean isEmpty() {
-//            return size == 0;
-//        }
-//
-//        public PriorityVal[] getData() {
-//            return data;
-//        }
-//
-//        public int getSize() {
-//            return size;
-//        }
-//
-//        private void percolateDown(PriorityVal[] a, int size, int i) {
-//            int left = (2 * i) + 1;
-//            int right = (2 * i) + 2;
-//
-//            int largest = i;
-//
-//            if (left < size && a[left].getPriority() > a[largest].getPriority()) {
-//                largest = left;
-//            }
-//
-//            if (right < size && a[right].getPriority() > a[largest].getPriority()) {
-//                largest = right;
-//            }
-//
-//            if (largest != i) {
-//                swap(largest, i);
-//                percolateDown(a, size, largest);
-//            }
-//        }
-//
-//        private void swap(int a, int b) {
-//        	PriorityVal temp = data[a];
-//            data[a] = data[b];
-//            data[b] = temp;
-//        }
-//    }
 
 }
 
--- WGraphTest.java
+++ WGraphTest.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 public class WGraphTest {
 	@Test
-    public void extractLinksTest1() {
+    public void v2vTest() {
 
         try {
 			WGraph wc = new WGraph("testGraph.txt");
@@ -16,4 +18,46 @@ public class WGraphTest {
 
       //  assertEquals(expected, actual);
     }
+	
+	@Test
+    public void v2sTest() {
+
+        try {
+			WGraph wc = new WGraph("testGraph.txt");
+			ArrayList<Integer> S = new ArrayList<Integer>();
+			S.add(7);
+			S.add(8);
+			wc.V2S(1, 2, S);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        
+
+      //  assertEquals(expected, actual);
+    }
+	
+	@Test
+    public void s2sTest() {
+
+        try {
+			WGraph wc = new WGraph("testGraph.txt");
+			ArrayList<Integer> S1 = new ArrayList<Integer>();
+			S1.add(1);
+			S1.add(2);
+			ArrayList<Integer> S2 = new ArrayList<Integer>();
+			S2.add(7);
+			S2.add(8);
+			wc.S2S(S1, S2);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        
+
+      //  assertEquals(expected, actual);
+    }
+	
 }

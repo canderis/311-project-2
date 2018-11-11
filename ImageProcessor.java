c9d45f9eb0af9c0e27e3fc1cc055ff6815aee9a5
John Jago <johnjago@protonmail.com>
2018-11-10 20:08:28 -0600

    Set up class and write method for computing importance

--- ImageProcessor.java
+++ ImageProcessor.java
@@ -1,3 +1,102 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ImageProcessor {
 
+  private String FName;
+  private int[][][] M;
+  private int height;
+  private int width;
+  private ArrayList<ArrayList<Integer>> I;
+
+  public ImageProcessor(String FName) {
+    this.FName = FName;
+
+    // Read in from file to create image matrix M
+    try {
+      this.generateImageMatrix();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    // Compute the importance matrix I
+    I = new ArrayList<>();
+
+    for (int i = 0; i < height; i++) {
+      I.add(new ArrayList<Integer>());
+      for (int j = 0; j < width; j++) {
+        I.get(i).add(importance(i, j));
+      }
+    }
+  }
+
+  public ArrayList<ArrayList<Integer>> getImportance() {
+    return this.I;
+  }
+
+  public void writeReduced(int k, String FName) {
+
+  }
+
+  private void generateImageMatrix() throws Exception {
+    Scanner in = new Scanner(new FileReader(this.FName));
+    this.height = in.nextInt();
+    this.width = in.nextInt();
+    M = new int[this.height][this.width][3];
+
+    for (int i = 0; i < this.height; i++) {
+      for (int j = 0; j < this.width; j++) {
+        for (int k = 0; k < 3; k++) {
+          this.M[i][j][k] = in.nextInt();
+        }
+      }
+    }
+  }
+
+  private int pDist(int[] p, int[] q) {
+    return (int) Math.pow(p[0] - q[0], 2) +
+           (int) Math.pow(p[1] - q[1], 2) +
+           (int) Math.pow(p[2] - q[2], 2);
+  }
+
+  private int importance(int i, int j) {
+    return xImportance(i, j) + yImportance(i, j);
+  }
+
+  private int yImportance(int i, int j) {
+    int[] p;
+    int[] q;
+
+    if (i == 0) {
+      p = this.M[height - 1][j];
+      q = this.M[i + 1][j];
+    } else if (i == this.height - 1) {
+      p = this.M[i - 1][j];
+      q = this.M[0][j];
+    } else {
+      p = this.M[i - 1][j];
+      q = this.M[i + 1][j];
+    }
+
+    return pDist(p, q);
+  }
+
+  private int xImportance(int i, int j) {
+    int[] p;
+    int[] q;
+
+    if (j == 0) {
+      p = this.M[i][width - 1];
+      q = this.M[i][j + 1];
+    } else if (j == this.width - 1) {
+      p = this.M[i][j - 1];
+      q = this.M[i][0];
+    } else {
+      p = this.M[i][j - 1];
+      q = this.M[i][j + 1];
+    }
+
+    return pDist(p, q);
+  }
 }
---
+++ ImageProcessorTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+public class ImageProcessorTest {
+
+  @Test
+  public void getImportanceTest() {
+    ImageProcessor imageProcessor = new ImageProcessor("testImage.txt");
+    ArrayList<ArrayList<Integer>> I = imageProcessor.getImportance();
+
+    assertEquals(8981, (int) I.get(0).get(0));
+    assertEquals(14067, (int) I.get(1).get(2));
+    assertEquals(33355, (int) I.get(2).get(3));
+  }
+
+  @Test
+  public void writeReducedTest() {
+
+  }
+}
